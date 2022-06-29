a7e42e2cab848292674b8bfe4523d8e5863ec33f
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-06-28 21:52:05 -0300

    AEEE

---
+++ app.js
@@ -0,0 +1,29 @@
+let comedy = [{
+    title: 'Free Guy',
+    director: 'Shawn Levy',
+    year: '2021'
+    
+}]
+let action = [{
+    title: 'Uncharted',
+    director: 'Ruben Fleischer',
+    year: '2022'
+    
+}]
+let fantasy = [{
+    title: 'Doctor Strange in the Multiverse of Madness',
+    director: 'Sam Raimi',
+    year: '2022'
+    
+}]
+console.log(comedy)
+console.log(action)
+console.log(fantasy)
+
+let movie = 'MIB'
+
+if(movie == 'MIB'){
+    console.log(`MY FAVORIT MOVIE IS ${movie}`)
+}else{
+    console.log('NOT EVEN 5 STARS..')
+}
